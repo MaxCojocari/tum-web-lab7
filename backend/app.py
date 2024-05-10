1b427caa3d96f5ea99f80a931884dc4e5237f172
MaxCojocari <cojocarigoncearmax@gmail.com>
2024-05-10 19:10:07 +0300

    feat: add endpoint for jwt generation

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from models.database import db
-from models.course import Course
+from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__)
     
     # Configure SQLAlchemy to use SQLite
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['JWT_SECRET_KEY'] = 'ww%JlM}wa&"q10' 
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 60
+    
+    JWTManager(app)
     db.init_app(app)
     return app
 
