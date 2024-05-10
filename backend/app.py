a3ddb784cbc632641b200c82d9236c59b7c31fe5
MaxCojocari <cojocarigoncearmax@gmail.com>
2024-05-10 20:54:57 +0300

    feat: integrate swagger (basic layout)

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from models.database import db
 from flask_jwt_extended import JWTManager
+from flasgger import Swagger
 
 def create_app():
     app = Flask(__name__)
     
-    # Configure SQLAlchemy to use SQLite
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     app.config['JWT_SECRET_KEY'] = 'ww%JlM}wa&"q10' 
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 60
-    
+    app.config['SWAGGER'] = {
+        "title": "CourseMe API",
+        "version": '0.1',
+        "description": "This is API description for CourseMe app.",
+        "license": {
+            "name": "Apache 2.0",
+            "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+        }
+    }
+    Swagger(app) 
     JWTManager(app)
     db.init_app(app)
     return app
